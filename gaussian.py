13164a1da6888542cf6054961adf744a1647fe2e
Peter Bloem <git@peterbloem.nl>
2017-10-24 10:15:31 +0200

    Test

--- gaussian.py
+++ gaussian.py
@@ -526,7 +526,7 @@ class ParamASHLayer(HyperLayer):
         Evaluates hypernetwork.
         """
 
-        print(type(self.params))
+        print(self.params)
 
         batch_size = input.size()[0]
 

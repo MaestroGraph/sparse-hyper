141ad73286fe0c3ebfe186abccd0a3d121cb635d
Peter Bloem <git@peterbloem.nl>
2017-10-31 14:57:06 +0100

    ...

---
+++ memleak2.py
@@ -0,0 +1,98 @@
+import torch
+from torch import FloatTensor, LongTensor
+from torch.autograd import Variable
+from torch import nn, optim
+from torch.nn import Parameter
+from tqdm import trange
+
+import util, logging, os, psutil
+
+import hyper, gaussian
+
+logging.basicConfig(filename='memleak.log',level=logging.INFO)
+
+torch.manual_seed(2)
+
+CUDA = False
+
+B = 256
+M = 32
+W, H = 2, 2048
+
+criterion = nn.MSELoss()
+
+FT = torch.cuda.sparse.FloatTensor if CUDA else torch.sparse.FloatTensor
+
+class SparseMult(torch.autograd.Function):
+
+    def __init__(self, use_cuda=False):
+        super().__init__()
+        self.use_cuda = use_cuda
+
+
+    def forward(self, indices, values, size, vector):
+
+        self.save_for_backward(indices, values, size, vector)
+
+        return res
+
+    def backward(self, grad_output):
+
+        indices, values, size, vector = self.saved_tensors
+        matrix = self.FT(indices, values, torch.Size(size))
+
+        i_ixs = indices[0,:]
+        j_ixs = indices[1,:]
+        output_select = grad_output.view(-1)[i_ixs]
+        vector_select = vector.view(-1)[j_ixs]
+
+        grad_values = output_select *  vector_select
+
+        grad_vector = torch.mm(grad_output.t(), matrix)
+        return None, grad_values, None, grad_vector
+
+def iteration():
+
+    sparsemult = util.SparseMult(use_cuda=CUDA)
+
+    mindices = (torch.rand(B, H, W) * M-1).long()
+    values = torch.rand(B, H)
+    bsize = LongTensor([M, M])
+    y_flat = torch.zeros(B, M)
+    x_flat = torch.rand(B, M)
+    target = x_flat.clone()
+
+    if CUDA:
+        mindices = mindices.cuda()
+        bsize = bsize.cuda()
+        values = values.cuda()
+        x_flat = x_flat.cuda()
+        y_flat = y_flat.cuda()
+        target = target.cuda()
+
+    bsize  = Variable(bsize)
+    values = Variable(values, requires_grad=True)
+    x_flat = Variable(x_flat)
+    y_flat = Variable(y_flat)
+    target = Variable(target)
+
+    if CUDA:
+        logging.info(util.nvidia_smi())
+
+    for b in range(B):
+        bindices = Variable(mindices[b, :, :].squeeze(0).t())
+        bvalues = values[b, :]
+
+        bx = x_flat[b, :]
+
+        matrix = FT(bindices.data, bvalues.data, torch.Size(bsize.data))
+        res = torch.mm(matrix, bx.data.unsqueeze(1))
+
+        y_flat[b, :] = res
+
+    loss = criterion(y_flat, target)
+
+    del sparsemult
+
+for i in trange(int(10e7)):
+    iteration()

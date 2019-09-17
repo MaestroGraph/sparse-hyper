1870f665f46b477cdb43d2f575c72739909206a7
Peter Bloem <git@peterbloem.nl>
2019-09-17 18:12:17 +0200

    Add cutout

--- experiments/convolution.py
+++ experiments/convolution.py
@@ -26,6 +26,50 @@ import matplotlib.pyplot as plt
 
 from util import d
 
+class Cutout(object):
+    """
+    Cutout transform. From https://github.com/uoguelph-mlrg/Cutout/blob/master/util/cutout.py
+
+    Randomly mask out one or more patches from an image.
+    Args:
+        n_holes (int): Number of patches to cut out of each image.
+        length (int): The length (in pixels) of each square patch.
+    """
+    def __init__(self, n_holes, length):
+        self.n_holes = n_holes
+        self.length = length
+
+    def __call__(self, img):
+        """
+        Args:
+            img (Tensor): Tensor image of size (C, H, W).
+        Returns:
+            Tensor: Image with n_holes of dimension length x length cut out of it.
+        """
+        h = img.size(1)
+        w = img.size(2)
+
+        mask = np.ones((h, w), np.float32)
+
+        for n in range(self.n_holes):
+            y = np.random.randint(h)
+            x = np.random.randint(w)
+
+            y1 = np.clip(y - self.length // 2, 0, h)
+            y2 = np.clip(y + self.length // 2, 0, h)
+            x1 = np.clip(x - self.length // 2, 0, w)
+            x2 = np.clip(x + self.length // 2, 0, w)
+
+            mask[y1: y2, x1: x2] = 0.
+
+        mask = torch.from_numpy(mask)
+        mask = mask.expand_as(img)
+        img = img * mask
+
+        return img
+
+
+
 class MobBlock(nn.Module):
     """
     Inverted residual block (as in mobilenetv2)
@@ -491,7 +535,7 @@ def go(arg):
 
         if arg.augmentation:
             tfms = transforms.Compose(
-                [transforms.RandomCrop(size=shape[1:], padding=4), transforms.RandomHorizontalFlip(), transforms.ToTensor()]
+                [transforms.RandomCrop(size=shape[1:], padding=4), transforms.RandomHorizontalFlip(), Cutout(1, 8), transforms.ToTensor()]
             )
 
         data = arg.data + os.sep + arg.task
@@ -520,7 +564,7 @@ def go(arg):
 
         if arg.augmentation:
             tfms = transforms.Compose(
-                [transforms.RandomCrop(size=shape[1:], padding=4), transforms.RandomHorizontalFlip(), transforms.ToTensor()]
+                [transforms.RandomCrop(size=shape[1:], padding=4), transforms.RandomHorizontalFlip(), Cutout(1, 8), transforms.ToTensor()]
             )
 
         data = arg.data + os.sep + arg.task

efd88b58fa370b784d67f952685b855f842cd8b5
Peter Bloem <git@peterbloem.nl>
2019-09-24 18:06:21 +0200

    Fix bug

--- sparse/__init__.py
+++ sparse/__init__.py
@@ -3,6 +3,6 @@ from .sort import Split, SortLayer
 from .layers import SparseLayer, NASLayer, Convolution, transform_means, transform_sigmas
 from .layers import ngenerate, transform_means, densities
 
-from .tensors import contract, logsoftmax, batchmm
+from .tensors import contract, logsoftmax, batchmm, simple_normalize
 
 # from .tensors import flatten_indices_mat
